de0d1db8c183bd7ec1938630645f464dc2a466f4
Yoyo shieh <yoyo.shieh@gmail.com>
2022-05-11 16:01:00 +0800

    yoyo

--- system/sign/main.php
+++ system/sign/main.php
@@ -2,8 +2,7 @@
   // 檢查cookie中的passed變數是否等於 TRUE
   $passed = $_COOKIE["passed"];
 	
-  /* 如果cookie中的passed變數不等於TRUE
-     表示尚未登入網站，將使用者導向首頁index.html */
+
   if ($passed != "TRUE")
   {
     header("location:index.html");
