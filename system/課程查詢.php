32e7f41f793b1d07471e789ab247acdd4258e018
teddywang <wtfwang23@gmail.com>
2022-05-12 16:36:29 +0800

    123

--- system/課程查詢.php
+++ system/課程查詢.php
@@ -1,16 +1,10 @@
+
 <meta  content="text/html; charset=utf-8"/>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-
-
-
-
-
 <!-- Navbar Light -->
 <nav
   class="navbar navbar-expand-lg navbar-light bg-white z-index-3 py-3">
@@ -106,7 +100,7 @@
 	    {
 		  $sql="select * from information where id like '%$searchtxt%' or password like '%$searchtxt%'";
 		  }
-      $rs=mysqli_query($link,$sql);;
+      $rs=mysqli_query($link,$sql);
       while($record=mysqli_fetch_row($rs))
       {
         echo "<tr>
