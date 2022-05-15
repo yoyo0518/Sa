20721c534f35dd08acd4c9c6cddb2cf47be9b7fc
Yoyo shieh <yoyo.shieh@gmail.com>
2022-05-15 11:52:00 +0800

    yoyo0515

--- system/課程查詢.php
+++ system/課程查詢.php
@@ -13,8 +13,10 @@ if (isset($_COOKIE["passed"]) && $_COOKIE["passed"]=='TRUE' ) {
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<?php if(isset($searchtxt)){
-$searchtxt=$_POST["searchtxt"];
+<?php 
+$searchtxt='';
+if(isset( $_POST["searchtxt"] )){
+  $searchtxt=$_POST["searchtxt"];
 }
 
 
@@ -164,7 +166,9 @@ if($passed != true){
       else
 	    {
 		  $sql="select * from information where 課程名稱 like '%$searchtxt%' or 課程代碼 like '%$searchtxt%' or 教師 like '%$searchtxt%' or 上課時間 like '%$searchtxt%'";
-		  }
+		
+
+    }
       $rs=mysqli_query($link,$sql);;
       while($record=mysqli_fetch_row($rs))
       {
