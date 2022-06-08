8917759196d9a6a1f8f9ad7ed60f166fe0936b81
teddywang <wtfwang23@gmail.com>
2022-06-08 17:25:35 +0800

    indexfooter

--- system/index.php
+++ system/index.php
@@ -190,177 +190,53 @@ if(empty($_SESSION["level"])){
 
   
 
-  <footer class="footer pt-5 mt-5">
-  <div class="container">
-    <div class=" row">
-      <div class="col-md-3 mb-4 ms-auto">
-        <div>
-          <a href="https://www.creative-tim.com/product/material-kit">
-            <img src="./assets/img/logo-ct-dark.png" class="mb-3 footer-logo" alt="main_logo">
-          </a>
-          <h6 class="font-weight-bolder mb-4">輔仁大學</h6>
-        </div>
-        <div>
-          <ul class="d-flex flex-row ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim" target="_blank">
-                <i class="fab fa-facebook text-lg opacity-8"></i>
-              </a>
-            </li>
+<footer class="bg-light text-center text-lg-start" >
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h4 class="text-uppercase"><strong>輔仁大學</strong></h5>
+         
 
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
-                <i class="fab fa-twitter text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://dribbble.com/creativetim" target="_blank">
-                <i class="fab fa-dribbble text-lg opacity-8"></i>
-              </a>
-            </li>
-
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://github.com/creativetimofficial" target="_blank">
-                <i class="fab fa-github text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w" target="_blank">
-                <i class="fab fa-youtube text-lg opacity-8"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
+       
       </div>
+      <!--Grid column-->
 
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+      <a href="https://www.instagram.com/irisxiao_/" class="text-dark">
+         <p class="text-uppercase mb-0">最新消息</p>
+      </a>
 
+        
+         
 
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">回報問題</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/presentation" target="_blank">
-                About Us
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/templates/free" target="_blank">
-                Freebies
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/templates/premium" target="_blank">
-                Premium Tools
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/blog" target="_blank">
-                Blog
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
+      <!--Grid column-->
 
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">聯絡我們</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://iradesign.io/" target="_blank">
-                Illustrations
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
-                Bits & Snippets
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/affiliates/new" target="_blank">
-                Affiliate Program
-              </a>
-            </li>
-          </ul>
-        </div>
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+      <a href="http://www.management.fju.edu.tw/subweb/donate/contact.php" class="text-dark"> 
+        <p class="text-uppercase">聯絡我們</p></a>
       </div>
+      <!--Grid column-->
 
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">製作單位</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/contact-us" target="_blank">
-                Contact Us
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center" target="_blank">
-                Knowledge Center
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-mk2-footer" target="_blank">
-                Custom Development
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/sponsorships" target="_blank">
-                Sponsorships
-              </a>
-            </li>
-
-          </ul>
-        </div>
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <p class="text-uppercase mb-0">製作單位</p>
+        <p>謝大毛股份有限公司</p>
       </div>
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
-        <div>
-          <h6 class="text-sm">最新消息</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/terms-of-service" target="_blank">
-                Terms & Conditions
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/privacy-policy" target="_blank">
-                Privacy Policy
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/license" target="_blank">
-                Licenses (EULA)
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-12">
-        <div class="text-center">
-          <p class="text-dark my-4 text-sm font-weight-normal">
-            All rights reserved. Copyright © <script>document.write(new Date().getFullYear())</script> Material Kit by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </p>
-        </div>
-      </div>
+      <!--Grid column-->
     </div>
+    <!--Grid row-->
   </div>
+  <!-- Grid container -->
+
+ 
 </footer>
+
 
 
   
